3b082013f4b6503d4f6284e42bd6698b9712fcd8
utkarsh.gupta <utkarsh.gupta@iongroup.com>
2025-12-30 14:30:26 +0530

    Commit transaction service

---
+++ fraud-detection/transaction-service-producer.py
@@ -0,0 +1,38 @@
+from confluent_kafka import Producer
+import uuid
+import json
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err: 
+        print('Delivery Report ERROR! -> ', err)
+    else: 
+        print("Delivery SUCCEEDED! -> ", {msg.value().decode('utf-8')})
+
+def get_transaction():
+    transaction = {
+        'transaction_id': str(uuid.uuid4()),
+        'amount': -500,
+        'currency': 'CHZ'
+    }
+
+    return json.dumps(transaction).encode('utf-8')
+
+def main():
+    for _ in range(3):
+        transaction = get_transaction()
+        producer.produce(
+            topic='transactions',
+            value=transaction,
+            callback=delivery_report
+        )
+    producer.flush()
+
+
+if __name__=="__main__":
+    main()
